45a8dfabe29d7f65b02bd9faea4e7df819cee65a
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2021-01-23 09:47:01 -0500

    Exámen Final

---
+++ src/main/java/co/edu/udea/microserviciosudearegistration/api/ContactApi.java
@@ -0,0 +1,17 @@
+package co.edu.udea.microserviciosudearegistration.api;
+
+import co.edu.udea.microserviciosudearegistration.dto.Contact;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class ContactApi {
+
+    @GetMapping(value = "/contact")
+    public Contact getContact(){
+        return new Contact(20L, "Luis", "Rios","+573216486632","lfernando.rios@udea.edu.co" );
+    }
+
+}
